bb3e225a1221627bebb018cb487080a84feb6d4c
Алексей <akarpushev@yandex.ru>
2024-02-02 10:04:07 +0300

    лаб. 3

--- Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
+++ Занятие 3. Инкапсуляция, наследование, полиморфизм/2. Лабораторные задания/task1/main.py
@@ -11,27 +11,15 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        self._name = name
-        self._author = author
-        self._pages = pages
+        self.__name = name
+        self.__author = author
+        self.pages = pages
 
     def __str__(self):
-        return f"Книга {self._name}. Автор {self._author}"
-
-    def is_valid(self, pages: int) -> None:
-        if not isinstance(pages, int):
-            raise TypeError
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def author(self):
-        return self._author
+        return f"Книга {self.__name}. Автор {self.__author}"
 
     @property
     def pages(self):
@@ -39,29 +27,41 @@ class PaperBook:
 
     @pages.setter
     def pages(self, value):
-        print('Вызван setter')
-        self.is_valid(value)
+        if not isinstance(value, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if value <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
         self._pages = value
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        self._name = name
-        self._author = author
+        self.__name = name
+        self.__author = author
         self.duration = duration
 
     def __str__(self):
-        return f"Книга {self._name}. Автор {self._author}"
-
-    def is_valid(self, duration: float) -> None:
-        if not isinstance(duration, float):
-            raise TypeError
+        return f"Книга {self.__name}. Автор {self.__author}"
 
     @property
-    def name(self):
-        return self._name
+    def duration(self):
+        return self._duration
 
-    @property
-    def author(self):
-        return self._author
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Длительность должна быть типа int")
+        if value <= 0:
+            raise ValueError("Длительность должна быть положительным числом")
+        self._duration = value
+
+
+# book = PaperBook('1', '5', 6)
+# print(book)
+# print([PaperBook('a', 's', i) for i in range(1, 5, 1)])
+# book.name = 'aa'
+# book.__name = 'aa'
+# print(book)
+# book.pages = 30
+# print(book.pages)
